b981511fc520784b6a43af4785cb900f62cb1e12
MARIENVAL Baptiste <bmarienval@softeam.com>
2016-01-06 17:58:21 +0100

    Ajout liste users

--- src/socket.js
+++ src/socket.js
@@ -7,6 +7,7 @@ var io = require('socket.io')(http);
 // Ces variables resteront durant toute la vie du seveur et sont communes pour chaque client (node server.js)
 // Liste des messages de la forme { pseudo : 'Mon pseudo', message : 'Mon message' }
 var messages = [];
+var users = [];
 
 
 app.get('/', function(req, res){
@@ -14,27 +15,31 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-   console.log('a user connected');
-   socket.emit('recupererMessages', messages);
+
+  socket.emit('updateUsersList', users);
 
    socket.on('disconnect', function(){
-      socket.broadcast.emit('disconnect');
+     console.log('disconnect');
+     var index = users.indexOf(socket.pseudo);
+     users.splice(index, 1);
+     io.emit('delUser', socket.pseudo);
+     io.emit('updateUsersList', users);
    });
 
-   socket.on('newUser', function(pseudo){
+   socket.on('newUser', function(pseudo){;
+    socket.pseudo = pseudo;
+    users.push(pseudo);
     io.emit('newUser', pseudo);
+    io.emit('updateUsersList', users);
    });
 
    socket.on('nouveauMessage', function(msg){
       console.log('message: ' + msg);
-      messages.push(msg);
       socket.broadcast.emit('recupererNouveauMessage', msg);
    });
 });
 
 
-
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
